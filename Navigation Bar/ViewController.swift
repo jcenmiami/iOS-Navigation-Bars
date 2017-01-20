c084f0108717ee6b5d5011e60bba5f9af969dcb3
Juan Ramirez <juanramirez@mail.usf.edu>
2017-01-20 14:29:44 -0500

    Added a Menu Bar at the top and footer.

--- Navigation Bar/ViewController.swift
+++ Navigation Bar/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func cameraButton(_ sender: Any) {
+        print("Camera Button Pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
